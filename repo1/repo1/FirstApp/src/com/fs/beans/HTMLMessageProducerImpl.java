1dc90ddd660ace3ea01d548842a1f674e6dc00a2
vinodkumar-un <vinodborlakunta@gmail.com>
2018-04-02 20:58:00 +0530

    'fc'

--- repo1/repo1/FirstApp/src/com/fs/beans/HTMLMessageProducerImpl.java
+++ repo1/repo1/FirstApp/src/com/fs/beans/HTMLMessageProducerImpl.java
@@ -4,7 +4,6 @@ public class HTMLMessageProducerImpl implements IMessageProducer {
 
 	@Override
 	public String convert(String message) {
-		String s = " with body. ";
 		return  "<html><body>" + message + "</body></html>";
 	}
 
